6069a0db4bde60125e5aefb5233178781a6d61c2
“rachelphilipose” <rachel@voltaric.com>
2024-02-04 15:52:34 -0500

    adding static sections for balances

---
+++ src/components/ExpenseTotal.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ExpenseTotal = () => {
+    return (
+        <div className= 'alert alert-info'>
+            <span>
+                Current Spending: $250
+            </span>
+        </div>
+    );
+};
+
+export default ExpenseTotal;
---
+++ src/components/Remaining.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Remaining = () => {
+    return(
+        <div className= 'alert alert-success'>
+            <span>
+                Remaining: $750
+            </span>
+        </div>
+    );
+};
+
+export default Remaining;
